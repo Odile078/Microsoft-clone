fcdcc476f1ade39741b587c4d98082869c3f6cd2
Odile078 <ouwimpuhwe620@daviscollege.com>
2022-09-12 09:14:18 -0700

    Implemeting page responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: ['./index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: { colors:{
+      lightgray:'#f2f2f2',
+      newblue: {
+        DEFAULT: "#0067b8",
+        dark: "#0067b8",
+      },
+      fontFamily:{
+        brandfamily:"Segoe UI,SegoeUI,Helvetica Neue,Helvetica,Arial,serif"
+      }
+    }},
   },
   variants: {
-    extend: {},
+    extend: {
+     
+    },
   },
   plugins: [],
 }
